2c2de951838f7346c76d543890d14c008fe1b32e
Marco Mazzai <mazzai.marco@gmail.com>
2025-08-19 08:55:02 +0200

    fix: migrate old code

--- src/content/content.ts
+++ src/content/content.ts
@@ -9,6 +9,75 @@ const blogCollection = defineCollection({
   }),
 });
 
+const copyCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    headline: z.string().optional(),
+    image: z
+      .object({
+        alt: z.string(),
+        src: z.string(),
+      })
+      .optional(),
+    content: z.string().optional(),
+    selectedWorks: z
+      .object({
+        headline: z.string(),
+        items: z.array(
+          z.object({
+            id: z.string(),
+            headline: z.string(),
+            type: z.string(),
+            description: z.string(),
+            tags: z.array(z.string()),
+            link: z.object({
+              label: z.string(),
+              url: z.string(),
+            }),
+          })
+        ),
+      })
+      .optional(),
+    nda: z
+      .object({
+        headline: z.string(),
+        images: z.array(
+          z.object({
+            alt: z.string(),
+            src: z.string(),
+          })
+        ),
+      })
+      .optional(),
+    knowMore: z
+      .object({
+        headline: z.string(),
+        CTA: z.array(
+          z.object({
+            label: z.string(),
+            url: z.string(),
+          })
+        ),
+      })
+      .optional(),
+    items: z
+      .array(
+        z.object({
+          label: z.string(),
+          url: z.string(),
+        })
+      )
+      .optional(),
+    CTA: z
+      .object({
+        label: z.string(),
+        url: z.string(),
+      })
+      .optional(),
+  }),
+});
+
 export const collections = {
   blog: blogCollection,
+  copy: copyCollection,
 };
--- src/content/copy/blog.md
+++ src/content/copy/blog.md
@@ -1,4 +1,3 @@
 ---
 headline: "Latest posts"
 ---
-
