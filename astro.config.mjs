7cee5fd9ecc2094d0de2d33a4a59539ab7f432aa
Marco Mazzai <mazzai.marco@gmail.com>
2026-08-12 16:32:43 +0200

    fix: scrap comments in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
+import stripHtmlComments from './integrations/strip-html-comments.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcomazzai.dev',
   integrations: [
+    stripHtmlComments(),
     icon({
       include: {
         ph: [
